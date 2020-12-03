f00e75bcba51a2eef4d122b35f77b247113dff14
Cloudbop <boppincolin@gmail.com>
2020-12-03 14:18:15 +0000

    added modules syntx

---
+++ src/modules/module-external.ts
@@ -0,0 +1,7 @@
+// expose on the namespace.
+export function displayName() {
+  return 'display some data ' + name;
+}
+
+
+// gets compiled to a modular IIFE that provides a namespaced object alongside window
---
+++ src/modules/module.runner.js
@@ -0,0 +1,6 @@
+import Object from "./module-external";
+
+// doesn't work, but should, reason???
+// import Object2 = require("./module-external.ts");
+
+console.log(Object.displayName());
